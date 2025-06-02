f32dca032f6e3118eef11865e3e749447a6f5083
mauri <mauricio.coronado08@gmail.com>
2025-06-01 20:54:09 -0500

    Refactor updateStatus method in TransactionRepository to include parameter annotations

--- src/main/java/com/example/tdd/domain/repository/TransactionRepository.java
+++ src/main/java/com/example/tdd/domain/repository/TransactionRepository.java
@@ -4,6 +4,7 @@ import com.example.tdd.domain.model.Transaction;
 import com.example.tdd.domain.enums.TransactionStatus;
 import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
@@ -11,5 +12,5 @@ public interface TransactionRepository extends ReactiveCrudRepository<Transactio
 
     @Modifying // Indica que esta query modifica datos
     @Query("UPDATE transaction SET status = :newStatus WHERE id = :id")
-    Mono<Integer> updateStatus(Long id, TransactionStatus newStatus);
+    Mono<Integer> updateStatus(@Param("id") Long id, @Param("newStatus") TransactionStatus newStatus);
 }
